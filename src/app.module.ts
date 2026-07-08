f12f270ab43a77004b5777dac36853b5a5ca9854
Ara Buenaventura <ara.buenaventura@cimb.com>
2026-07-08 11:16:55 +0800

    --updated: apply GUARD via Arcjet(main, appmodule)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {
+  ArcjetGuard,
+  ArcjetModule,
+  fixedWindow,
+  shield,
+} from "@arcjet/nest";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
-  imports: [],
+imports: [
+    ArcjetModule.forRoot({
+      isGlobal: true,
+      key: process.env.ARCJET_KEY!,
+      rules: [
+        shield({ mode: "LIVE" }),
+        fixedWindow({
+          mode: "LIVE",
+          window: "60s",
+          max: 2,
+        }),
+      ],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ArcjetGuard,
+    },
+  ],
 })
 export class AppModule {}
